ec1142020ad2c4a6da5da8a3d16474993023de89
CristianG5 <cristianse.galvan@gmail.com>
2024-06-11 04:59:12 -0300

    first commit

---
+++ client/js/product.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id:1,
+        productName: "Banana",
+        price: 500,
+        quanty: 5,
+        img:"https://acdn.mitiendanube.com/stores/001/219/229/products/banana1-39d62a544fc16a2b8016025979095295-640-0.jpg",
+    },
+    {
+        id:2,
+        productName: "Durazno",
+        price: 500,
+        quanty: 5,
+        img:"https://www.grupoalta.com/wp-content/uploads/2018/03/Productos_06.png",
+    },
+    {
+        id:2,
+        productName: "Frutilla",
+        price: 500,
+        quanty: 5,
+        img:"https://assets.elgourmet.com/wp-content/uploads/2023/03/diccio_133.jpg.webp",
+    },
+    {
+        id:4,
+        productName: "Manzana",
+        price: 500,
+        quanty: 5,
+        img:"https://www.recetasnestlecam.com/sites/default/files/2022-04/tipos-de-manzana-royal-gala.jpg",
+    },
+    {
+        id:5,
+        productName: "Naranja",
+        price: 500,
+        quanty: 5,
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnOPpqg_qUmLFDiWUuvT3SvtoLGNoak1R3tPe-xM3tcw&s",
+    },
+]
