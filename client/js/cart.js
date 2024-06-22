7cc0f556d4a453ad70984fd7946874ea8398800a
CristianG5 <cristianse.galvan@gmail.com>
2024-06-22 03:24:47 -0300

    Front Completado

--- client/js/cart.js
+++ client/js/cart.js
@@ -2,6 +2,7 @@ const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
 const cartBtn = document.getElementById("cart-btn")
+const cartCounter = document.getElementById("cart-counter")
 
 const displayCart = () => {
     modalContainer.innerHTML = "";
@@ -25,8 +26,88 @@ const displayCart = () => {
     modalHeader.append(modalTitle);
 
     modalContainer.append(modalHeader); 
-    
+
+    if(cart.length > 0){
+    cart.forEach((productos)=>{
+        const modalBody = document.createElement("div");
+        modalBody.className = "modal-body";
+        modalBody.innerHTML = `
+            <div class = "product">
+                <img class="product-img" src= "${productos.img}" />
+                <div class="product-info">
+                    <h4>${productos.productName}</h4>
+            </div>
+
+            <div class = "quantity">
+                <span class="quantity-btn-decrese">-</span>
+                <span class="quantity-input">${productos.quantity}</span>
+                <span class="quantity-btn-increse">+</span>
+            </div>
+
+            <div class="price">${productos.price * productos.quantity} $</div>    
+                <div class="delete-product">❌</div>
+            </div>    
+        `;
+        modalContainer.append(modalBody);
+
+        const decrese = modalBody.querySelector(".quantity-btn-decrese");
+        decrese.addEventListener("click", () => {
+            if(productos.quantity !== 1){
+                productos.quantity--;
+                displayCart();
+            }
+            displayCartCounter();
+        });   
+        
+        const increse = modalBody.querySelector(".quantity-btn-increse");
+        increse.addEventListener("click", ()=>{
+            productos.quantity++;
+            displayCart();
+            displayCartCounter();
+        })
+
+        const deleteProduct = modalBody.querySelector(".delete-product");
+        deleteProduct.addEventListener("click", ()=>{
+            deleteCartProduct(productos.id);
+        })
+
+    });
+
+
+
+    const total = cart.reduce((acc, el)=> acc + el.price * el.quantity, 0);
+
+    const modalFooter = document.createElement("div");
+    modalFooter.className= "modal-footer";
+    modalFooter.innerHTML= `
+        <div class = "total-price">Total: ${total}</div>
+    `;
+    modalContainer.append(modalFooter)
+    }else{
+        const modalText = document.createElement("h2");
+        modalText.className = "modal-body";
+        modalText.innerText = "Tu carrito esta vacio";
+        modalContainer.append(modalText);
+    }
 };
 
 
 cartBtn.addEventListener("click", displayCart)
+
+const deleteCartProduct = (id)=>{
+    const foundId = cart.findIndex((element)=> element.id === id);
+    console.log(foundId);
+    cart.splice(foundId, 1);
+    displayCart();
+    displayCartCounter();
+};
+
+const displayCartCounter = ()=>{
+    const cartLength = cart.reduce((acc, el)=> acc + el.quantity, 0)
+    if(cartLength > 0){
+        cartCounter.style.display = "block";
+        cartCounter.innerText = cartLength;
+    }else{
+        cartCounter.style.display = "none";       
+    }
+}
--- client/js/index.js
+++ client/js/index.js
@@ -16,14 +16,24 @@ productos.forEach((product)=>{
     content.append(buyButton);
 
     buyButton.addEventListener("click", () => {
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
+        const repeat = cart.some((repeatProduct)=> repeatProduct.id === product.id)
 
-        })
-        console.log(cart);
-    })
-})
+        if(repeat){
+            cart.map((prod)=>{
+                if(prod.id === product.id){
+                    prod.quantity++;
+                    displayCartCounter();
+                }
+            });
+        }else{
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quantity: product.quantity,
+                img: product.img,
+            });
+            displayCartCounter();
+        }
+    });
+});
